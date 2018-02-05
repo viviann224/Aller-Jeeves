b259ca9a5c36e4fb8a0c479c01c5a685c0202528
kyleCarter <kcarter625@gmail.com>
2018-02-05 13:28:13 -0600

    branch files

---
+++ assets/jeeves.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDzH6GmH4AivIfB6ukE5sxHQ6vSPFOlfVQ",
+    authDomain: "project-one-80dc8.firebaseapp.com",
+    databaseURL: "https://project-one-80dc8.firebaseio.com",
+    projectId: "project-one-80dc8",
+    storageBucket: "",
+    messagingSenderId: "195526743097"
+  };
+  firebase.initializeApp(config);
