a102885a113f9445d9a4cf302b67574488e33cc7
viviann224 <viviann224@gmail.com>
2018-02-06 12:06:38 -0600

    updated api method fx

--- assets/js/jeeves.js
+++ assets/js/jeeves.js
@@ -2,6 +2,13 @@
 // Edamam API key: f0230ede31a4d5ec34b6d43a4d98c3e2
 // Edamam group ID: 8bee9814
 // foodtofork: dd38b57cf59482a6f6a4907bfa20c71f
+
+//Yummly
+//appID: 87e47442
+//keyID: 11e4aadcc3dddb10fa26ae2968e1ce03
+
+//get: http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your _search_parameters
+//search: http://api.yummly.com/v1/api/recipes?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY&q=onion+soup
 console.log("I'm linked");
 
 // Initialize Firebase
@@ -20,83 +27,81 @@ var database = firebase.database();
 
 
 // function edamamQuery () {
-// 	$.ajax({
-//     	url: queryURL,
-//     	method: "GET"
-// 	}).then(function(argument)) {
+//  $.ajax({
+//      url: queryURL,
+//      method: "GET"
+//  }).then(function(argument)) {
 
-// 	}
+//  }
 // }
 //creates url query
 
 //creates an array of recipe id's that matches with the user input
 function foodToForkURL(userinput)
 {
-    var myURL="http://api.yummly.com/v1/api/recipes?_app_id=87e47442&_app_key=11e4aadcc3dddb10fa26ae2968e1ce03&q="+userinput;
-   //creates an empty array to store the recipe id's
-   var myArray= [];
+  //yummily key
+  var apiKey="11e4aadcc3dddb10fa26ae2968e1ce03";
+  //yumily app id
+  var appid="87e47442";
+  //url creation
+  var myURL="http://api.yummly.com/v1/api/recipes?_app_id="+appid+"&_app_key="+apiKey+"&q="+userinput;
+  //creates an empty array to store recipe title
+   var myTitle= [];
+   var myRecURL= [];
+   var myRecImg =[];
    //variable to hold the recipe name in lowercase
     var lowerCheck;
+    //string to change imge
+    var temp;
+
+    
     //calling the ajax class to pass the url, and the
     //GET method to return the myObj object
     $.ajax({
       url:myURL,
       method:"GET"
-    //once myString string returns, pass in myString to the next function
-    }).then(function(myString)
+    }).then(function(myObj)
     {
-      console.log(myString.matches[0].ingredients);
-      /*
-      //convert myString into an object via JSON function
-       var newObj =JSON.parse(myString);
-
-       for(var x=0; x<newObj.recipes.length; x++)
-       {
-        //removing case sensitivy on title of recipe
-        lowerCheck=newObj.recipes[x].title.toLowerCase();
-         //check the user input against the title if the keyword is the same add to array
+        //creates size variable for each search
+        var size =myObj.matches.length;
+        for(var x=0; x<size; x++)
+        {
+          //fix case sensitivity to compare with title
+          lowerCheck= myObj.matches[x].id.toLowerCase()
+          console.log(lowerCheck);
+          console.log(lowerCheck.includes(userinput));
+          //if title contains user input create an array to store the recipe, image, and url
           if(lowerCheck.includes(userinput))
           {
-            //need to convert object to a string with recipe ID **TO DO
-            var strRecId=JSON.stringify(parseInt(newObj.recipes[x].recipe_id));
-            myArray.push(strRecId);
-
+            //make title array
+            myTitle.push(myObj.matches[x].id);
+            //make url array
+            myRecURL.push("https://www.yummly.com/recipe/"+myObj.matches[x].id);
+            //makes resized image from image object 
+            temp= JSON.stringify(myObj.matches[x].imageUrlsBySize[90]);
+            console.log(typeof(temp));
+            temp=temp.replace("s90", "s500");
+            console.log("change size: "+temp);
+            //stores a string of url address
+            myRecImg.push(temp);
+            $(".container").append("<img src="+myRecImg[x]+" id='yumImg'>;");
           }
-       } 
-       */
-    });
-    //returns object array
 
-    return myArray;
+        }
+        console.log(myTitle);
+        console.log(myRecURL);
+        console.log(myRecImg);
+    });
 }
 
 //once get userinput with the associated array of recipe id's get the ingredient list
 function foodToForkDisplayRec(userinput, arrayRec) 
 {
-  var id;
-  console.log(arrayRec.length);
-  for(var x=0;x<arrayRec.length; x++)
-  {
-    id=arrayRec[x];
-  var myURL="http://api.yummly.com/v1/api/recipes?_app_id=87e47442&_app_key=11e4aadcc3dddb10fa26ae2968e1ce03&your%20_search_parameters";
-
-  console.log(myURL);
-      $.ajax({
-      url:myURL,
-      method:"GET"
-     }).then(function(myString)
-    {
-        //convert myString into an object via JSON function
-        var newObj =JSON.parse(myString);
-       
-   });
-
-  }
    
 }
 
 var arrayRecId= foodToForkURL("chicken");
-console.log(arrayRecId);
+//console.log(arrayRecId);
 
 
-foodToForkDisplayRec("chicken", arrayRecId);
+//foodToForkDisplayRec("chicken", arrayRecId);
